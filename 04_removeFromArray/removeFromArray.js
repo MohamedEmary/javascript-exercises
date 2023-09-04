0ceed2b7c8b53d03beb8eb9cc716952b95e592b2
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-09-04 15:27:42 +0300

    solve exercise four

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+// // One way using a new array and a for loop and add elements
+// // of arr to new array if they are not present in `removeElements`
+// const removeFromArray = function (arr, ...removeElements) {
+//   let newArr = [];
+//   for (let i of arr) {
+//     if (removeElements.includes(i) === true) {
+//       continue;
+//     }
+//     newArr.push(i);
+//   }
+//   return newArr;
+// };
 
+// Another Way is just to delete the elements of removeElements
+// from Arr
+const removeFromArray = function (arr, ...removeElements) {
+  for (let i of removeElements) {
+    while (arr.indexOf(i) !== -1) {
+      arr.splice(arr.indexOf(i), 1); // remove that element from the array
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
