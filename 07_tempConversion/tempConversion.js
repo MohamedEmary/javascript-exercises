95f76def343605a9127802958688110ecff8d8e0
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-09-04 16:36:20 +0300

    solve exercise seven

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F) {
+  const C = (F - 32) / 1.8;
+  return +C.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (C) {
+  let F = C * 1.8 + 32;
+  return +F.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
