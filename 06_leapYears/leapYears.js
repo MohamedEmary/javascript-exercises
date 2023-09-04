129a6b2d5af3e3b46259a4026784c43dbba0fb65
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-09-04 16:25:09 +0300

    add comment before function `return` statement

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,9 @@
 const leapYears = function (year) {
   if (typeof year !== "number") return "ERROR";
+  // A leap year is divisible by 4, except if it
+  // is also divisible by 100 and not divisible by 400
   return year % 4 === 0 && (year < 100 || year % 100 !== 0 || year % 400 === 0);
 };
-
-console.log(leapYears());
 
 // Do not edit below this line
 module.exports = leapYears;
