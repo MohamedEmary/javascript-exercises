0c5201f212d96dc22db720b717f29e053c652e70
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-09-04 15:00:50 +0300

    solve exercise three

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+// // One way using a for loop and a variable
+// const reverseString = function (string) {
+//   let reversedStr = "";
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     reversedStr += string[i];
+//   }
+//   return reversedStr;
+// };
 
+// Another way using split and reverse functions
+const reverseString = function (string) {
+  string = string.split("");
+  string = string.reverse().join("");
+  return string;
 };
+
+console.log(reverseString("mohamed"));
 
 // Do not edit below this line
 module.exports = reverseString;
