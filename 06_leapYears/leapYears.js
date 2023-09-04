14918de3b52f335a534e6d652bd07a19a55e17fd
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-09-04 16:21:00 +0300

    solve exercise six

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (typeof year !== "number") return "ERROR";
+  return year % 4 === 0 && (year < 100 || year % 100 !== 0 || year % 400 === 0);
 };
+
+console.log(leapYears());
 
 // Do not edit below this line
 module.exports = leapYears;
