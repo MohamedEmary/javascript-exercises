7c0f57415dd4ea5c0d5bdde48e7a1c9d29eac94d
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-09-23 21:54:51 +0300

    solve exercise twelve

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
+  people.forEach((personObj) => {
+    let Death = personObj["yearOfDeath"];
+    if (Death === undefined) {
+      personObj["yearOfDeath"] = currentYear;
+    }
+    return personObj;
+  });
+  let peopleWithAge = people.map((personObj) => {
+    return (personObj = {
+      name: personObj.name,
+      age: personObj.yearOfDeath - personObj.yearOfBirth,
+    });
+  });
+  const oldest = peopleWithAge.reduce((currPersonObj, nextPersonObj) => {
+    if (currPersonObj.age > nextPersonObj.age) return currPersonObj;
+    else return nextPersonObj;
+  }, 0);
+  return oldest;
 };
 
 // Do not edit below this line
