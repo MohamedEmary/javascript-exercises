19a6c9d5a1b678c1c930ffafc917adb9eebd7acf
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-09-23 20:21:09 +0300

    solve exercise nine

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string = "") {
+  string = string
+    .toLowerCase()
+    .split("")
+    .filter((ch) => {
+      const punctuation = [" ", ",", ".", "!"];
+      return !punctuation.includes(ch);
+    })
+    .join("");
+  string;
+  let reveredStr = [...string].reverse().join("");
+  return string === reveredStr;
 };
 
 // Do not edit below this line
