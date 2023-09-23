686559b46f0798581bf57a30a7b28c728e33dcb4
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-09-23 20:48:10 +0300

    solve exercise ten

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  let fibonacciArr = [1];
+  let curr = 1;
+  for (let i = 1; i < num; i++) {
+    fibonacciArr.push(curr);
+    curr =
+      fibonacciArr[fibonacciArr.length - 1] +
+      fibonacciArr[fibonacciArr.length - 2];
+  }
+  return fibonacciArr[num - 1];
 };
 
 // Do not edit below this line
